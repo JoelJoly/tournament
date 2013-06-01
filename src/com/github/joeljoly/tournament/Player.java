4b94f617aa53c6631030661accc955bce4dc1f0d
Joël Joly <joel.joly@gmail.com>
2013-06-02 01:28:32 +0200

    Add Player class
    to manipulate player data.

---
+++ src/com/github/joeljoly/tournament/Player.java
@@ -0,0 +1,58 @@
+package com.github.joeljoly.tournament;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: joel
+ * Date: 6/2/13
+ * Time: 12:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Player {
+    private String firstName;
+    private String lastName;
+    private Integer id;
+    private Integer points;
+
+    public Player(Integer id_, String firstName, String lastName, Integer points) {
+        this.id = id_;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.points = points;
+    }
+
+    public Player(Integer id) {
+        this(id, "John", "Doe", 500);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
