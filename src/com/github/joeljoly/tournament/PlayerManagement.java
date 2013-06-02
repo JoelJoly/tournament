ef3a58f1e281c722934425d1947dfbac47254ff9
Joël Joly <joel.joly@gmail.com>
2013-06-02 20:41:12 +0200

    "Add player" action
    icon from http://www.iconspedia.com/icon/sub-black-add-1488-.html

--- src/com/github/joeljoly/tournament/PlayerManagement.java
+++ src/com/github/joeljoly/tournament/PlayerManagement.java
@@ -1,7 +1,10 @@
 package com.github.joeljoly.tournament;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,4 +17,22 @@ public class PlayerManagement extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.player_management_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_player:
+                // app icon in action bar clicked; go home
+//                Intent intent = new Intent(this, CreatePlayer.class);
+//                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }}
