c3cc836bff3cdcc684abadd5b9c1eb13b640154a
Joël Joly <joel.joly@gmail.com>
2013-11-05 22:55:37 +0100

    Load session cookie
    use web client to load the page containing the cookie for further
    requests.

--- Tournament/src/main/java/com/github/joeljoly/tournament/ScrapingActivity.java
+++ Tournament/src/main/java/com/github/joeljoly/tournament/ScrapingActivity.java
@@ -1,5 +1,7 @@
 package com.github.joeljoly.tournament;
 
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -8,12 +10,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.util.List;
 
 public class ScrapingActivity extends ActionBarActivity {
+
+    /**
+     * Flag to know if the activity supports a progress bar.
+     */
+    boolean mHasFeatureProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mHasFeatureProgress = requestWindowFeature(Window.FEATURE_PROGRESS);
         setContentView(R.layout.activity_scraping);
 
         if (savedInstanceState == null) {
@@ -22,7 +39,6 @@ public class ScrapingActivity extends ActionBarActivity {
                     .commit();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,6 +64,51 @@ public class ScrapingActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        /**
+         * The HTTP client used in all transactions.
+         */
+        DefaultHttpClient mClient;
+
+        private class CookieRetrieval extends AsyncTask<String, Integer, List<Cookie>> {
+            private Exception mExceptionDuringBackground;
+            private Activity mActivity;
+
+            public CookieRetrieval(Activity activity) {
+                mActivity = activity;
+            }
+            @Override
+            protected List<Cookie> doInBackground(String... urls) {
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet(urls[0]);
+                try {
+                    HttpResponse response = client.execute(request);
+                } catch (Exception e) {
+                    mExceptionDuringBackground = e;
+                    cancel(true);
+                }
+                return client.getCookieStore().getCookies();
+            }
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                super.onProgressUpdate(values);
+                mActivity.setProgress(values[0] * 1000);
+            }
+
+            @Override
+            protected void onPostExecute(List<Cookie> cookies) {
+                super.onPostExecute(cookies);
+                for (Cookie cookie : cookies) {
+                    Toast.makeText(mActivity, "cookie " + cookie.getName() + " = " + cookie.getValue(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            protected void onCancelled(List<Cookie> cookies) {
+                super.onCancelled(cookies);
+                if (mExceptionDuringBackground != null)
+                    Toast.makeText(mActivity, "Connexion error error" + mExceptionDuringBackground.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
 
         public PlaceholderFragment() {
         }
@@ -55,7 +116,14 @@ public class ScrapingActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
+            super.onCreateView(inflater, container, savedInstanceState);
             View rootView = inflater.inflate(R.layout.fragment_scraping, container, false);
+
+            CookieRetrieval retrieveCookie = new CookieRetrieval(getActivity());
+            retrieveCookie.execute(getString(R.string.scraping_web_target));
+            if (((ScrapingActivity)getActivity()).mHasFeatureProgress) {
+            }
+
             return rootView;
         }
     }
