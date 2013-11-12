372217c7833d4912864a075ff1bf82a11590b7e6
Joël Joly <joel.joly@gmail.com>
2013-11-12 14:00:59 +0100

    Use headless retained fragment
    so that HTPP connection and data retrieval is not reset on mode change
    (e.g. portrait/landscape mode).

--- Tournament/src/main/java/com/github/joeljoly/tournament/ScrapingActivity.java
+++ Tournament/src/main/java/com/github/joeljoly/tournament/ScrapingActivity.java
@@ -1,7 +1,7 @@
 package com.github.joeljoly.tournament;
 
-import android.app.Activity;
 import android.os.AsyncTask;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -16,6 +16,7 @@ import android.widget.Toast;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.AbstractHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.util.List;
@@ -64,51 +65,7 @@ public class ScrapingActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The HTTP client used in all transactions.
-         */
-        DefaultHttpClient mClient;
-
-        private class CookieRetrieval extends AsyncTask<String, Integer, List<Cookie>> {
-            private Exception mExceptionDuringBackground;
-            private Activity mActivity;
-
-            public CookieRetrieval(Activity activity) {
-                mActivity = activity;
-            }
-            @Override
-            protected List<Cookie> doInBackground(String... urls) {
-                DefaultHttpClient client = new DefaultHttpClient();
-                HttpGet request = new HttpGet(urls[0]);
-                try {
-                    HttpResponse response = client.execute(request);
-                } catch (Exception e) {
-                    mExceptionDuringBackground = e;
-                    cancel(true);
-                }
-                return client.getCookieStore().getCookies();
-            }
-            @Override
-            protected void onProgressUpdate(Integer... values) {
-                super.onProgressUpdate(values);
-                mActivity.setProgress(values[0] * 1000);
-            }
-
-            @Override
-            protected void onPostExecute(List<Cookie> cookies) {
-                super.onPostExecute(cookies);
-                for (Cookie cookie : cookies) {
-                    Toast.makeText(mActivity, "cookie " + cookie.getName() + " = " + cookie.getValue(), Toast.LENGTH_LONG).show();
-                }
-            }
-
-            @Override
-            protected void onCancelled(List<Cookie> cookies) {
-                super.onCancelled(cookies);
-                if (mExceptionDuringBackground != null)
-                    Toast.makeText(mActivity, "Connexion error error" + mExceptionDuringBackground.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        }
+        static int httpRequestIntentCode = 1;
 
         public PlaceholderFragment() {
         }
@@ -117,15 +74,84 @@ public class ScrapingActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             super.onCreateView(inflater, container, savedInstanceState);
-            View rootView = inflater.inflate(R.layout.fragment_scraping, container, false);
 
-            CookieRetrieval retrieveCookie = new CookieRetrieval(getActivity());
+            FragmentManager fragmentManager = getFragmentManager();
+            Fragment fragment = fragmentManager.findFragmentByTag(HttpRequestsFragment.TAG);
+            if (fragment == null) {
+                fragment = new HttpRequestsFragment();
+                fragment.setTargetFragment(this, httpRequestIntentCode);
+                fragmentManager.beginTransaction().add(fragment,
+                        HttpRequestsFragment.TAG).commit();
+            }
+
+            View rootView = inflater.inflate(R.layout.fragment_scraping, container, false);
+            return rootView;
+        }
+    }
+
+    /**
+     * A headless retained fragment for handling HTTP transactions.
+     */
+    public static class HttpRequestsFragment extends Fragment {
+
+        static String TAG = "HttpRequestsFragment.instance";
+        /**
+         * The HTTP client used in all transactions.
+         */
+        private AbstractHttpClient mClient;
+
+        private class CookieRetrieval extends AsyncTask<String, Integer, List<Cookie>> {
+            private Exception mExceptionDuringBackground;
+
+            public CookieRetrieval(AbstractHttpClient client) {
+                mClient = client;
+            }
+            @Override
+            protected List<Cookie> doInBackground(String... urls) {
+                HttpGet request = new HttpGet(urls[0]);
+                try {
+                    HttpResponse response = mClient.execute(request);
+                } catch (Exception e) {
+                    mExceptionDuringBackground = e;
+                    cancel(true);
+                }
+                return mClient.getCookieStore().getCookies();
+            }
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                super.onProgressUpdate(values);
+                getActivity().setProgress(values[0] * 1000);
+            }
+
+            @Override
+            protected void onPostExecute(List<Cookie> cookies) {
+                super.onPostExecute(cookies);
+                for (Cookie cookie : cookies) {
+                    Toast.makeText(getActivity(), "cookie " + cookie.getName() + " = " + cookie.getValue(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            protected void onCancelled(List<Cookie> cookies) {
+                super.onCancelled(cookies);
+                if (mExceptionDuringBackground != null)
+                    Toast.makeText(getActivity(), "Connexion error error" + mExceptionDuringBackground.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+        HttpRequestsFragment() {
+            mClient = new DefaultHttpClient();
+        }
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // headless and retained fragment to not reset on mode changes
+            setRetainInstance(true);
+            CookieRetrieval retrieveCookie = new CookieRetrieval(mClient);
             retrieveCookie.execute(getString(R.string.scraping_web_target));
             if (((ScrapingActivity)getActivity()).mHasFeatureProgress) {
             }
-
-            return rootView;
         }
+
     }
 
 }
